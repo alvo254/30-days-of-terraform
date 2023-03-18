d28a42473c8899d8ffe06ff6dc1307568ce0bb9c
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-03-18 19:33:32 +0300

    CDN end point example

--- intro/main.tf
+++ intro/main.tf
@@ -18,3 +18,26 @@ resource "azurerm_resource_group" "rg" {
   location = "westus2"
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_cdn_profile" "example" {
+  name                = "example-cdn"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard_Verizon"
+}
+
+resource "azurerm_cdn_endpoint" "example" {
+  name                = "example"
+  profile_name        = azurerm_cdn_profile.example.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  origin {
+    name      = "example"
+    host_name = "www.contoso.com"
+  }
+}
