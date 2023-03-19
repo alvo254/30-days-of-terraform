bb58190b5b2edf1e2141d7dc6b650fa70672d993
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-03-19 12:51:18 +0300

    cognitive

--- intro/main.tf
+++ intro/main.tf
@@ -41,3 +41,17 @@ resource "azurerm_cdn_endpoint" "example" {
     host_name = "www.contoso.com"
   }
 }
+
+
+resource "azurerm_cognitive_account" "example" {
+  name                = "example-account"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Face"
+
+  sku_name = "S0"
+
+  tags = {
+    Acceptance = "Test"
+  }
+}
