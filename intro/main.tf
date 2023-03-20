b2206166d260c9af2cd2ca13cbe44a014315de74
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-03-20 20:37:08 +0300

    managed keys

--- intro/main.tf
+++ intro/main.tf
@@ -55,3 +55,85 @@ resource "azurerm_cognitive_account" "example" {
     Acceptance = "Test"
   }
 }
+
+
+data "azurerm_client_config" "current" {}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West US"
+}
+
+resource "azurerm_user_assigned_identity" "example" {
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  name                = "example-identity"
+}
+
+resource "azurerm_cognitive_account" "example" {
+  name                  = "example-account"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  kind                  = "Face"
+  sku_name              = "E0"
+  custom_subdomain_name = "example-account"
+  identity {
+    type         = "SystemAssigned, UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.example.id]
+  }
+}
+
+resource "azurerm_key_vault" "example" {
+  name                     = "example-vault"
+  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.example.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = true
+
+  access_policy {
+    tenant_id = azurerm_cognitive_account.example.identity.0.tenant_id
+    object_id = azurerm_cognitive_account.example.identity.0.principal_id
+    key_permissions = [
+      "Get", "Create", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"
+    ]
+    secret_permissions = [
+      "Get",
+    ]
+  }
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"
+    ]
+    secret_permissions = [
+      "Get",
+    ]
+  }
+
+  access_policy {
+    tenant_id = azurerm_user_assigned_identity.example.tenant_id
+    object_id = azurerm_user_assigned_identity.example.principal_id
+    key_permissions = [
+      "Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"
+    ]
+    secret_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_key" "example" {
+  name         = "example-key"
+  key_vault_id = azurerm_key_vault.example.id
+  key_type     = "RSA"
+  key_size     = 2048
+  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
+}
+
+resource "azurerm_cognitive_account_customer_managed_key" "example" {
+  cognitive_account_id = azurerm_cognitive_account.example.id
+  key_vault_key_id     = azurerm_key_vault_key.example.id
+  identity_client_id   = azurerm_user_assigned_identity.example.client_id
+}
