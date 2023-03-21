54cd28e1f71692732e02a4c77b621a27f42f2215
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-03-21 20:05:02 +0300

    availability set

--- intro/main.tf
+++ intro/main.tf
@@ -137,3 +137,15 @@ resource "azurerm_cognitive_account_customer_managed_key" "example" {
   key_vault_key_id     = azurerm_key_vault_key.example.id
   identity_client_id   = azurerm_user_assigned_identity.example.client_id
 }
+
+
+
+resource "azurerm_availability_set" "example" {
+  name                = "example-aset"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  tags = {
+    environment = "Production"
+  }
+}
